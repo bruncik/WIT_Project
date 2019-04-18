23c37f4abdbd68405fd6b8ade5e4b4525c117022
Jaroslav Bruncik <jarobruncik@gmail.com>
2019-04-18 17:14:00 +0100

    Adding Empty Activities

---
+++ app/src/main/java/com/example/wit_project/CreateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wit_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
---
+++ app/src/main/java/com/example/wit_project/ReadActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wit_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
