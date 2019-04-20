398c301a0eed9e55b6c4b9a0001253bdb3553afa
Jaroslav Bruncik <jarobruncik@gmail.com>
2019-04-20 10:28:30 +0100

    Testing inputs

--- app/src/main/java/com/example/wit_project/CreateActivity.kt
+++ app/src/main/java/com/example/wit_project/CreateActivity.kt
@@ -1,12 +1,31 @@
 package com.example.wit_project
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create.*
+
+val PRIVATE_MODE = 0
+val Pref_Name : String = "witApp"
 
 class CreateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        btnCreateActivity.setOnClickListener {
+            // Taking the data from textView
+            val title = edtTitle.text.toString()
+            val data = edtData.text.toString()
+
+            // SharedPreferences area
+            val sharedPref: SharedPreferences = getSharedPreferences(Pref_Name, PRIVATE_MODE)
+            val editor = sharedPref.edit()
+            editor.putString(title,data)
+            editor.apply()
+
+        }
+
     }
 }
--- app/src/main/java/com/example/wit_project/ReadActivity.kt
+++ app/src/main/java/com/example/wit_project/ReadActivity.kt
@@ -1,12 +1,23 @@
 package com.example.wit_project
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_read.*
 
 class ReadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read)
+
+        btnReadActivity.setOnClickListener {
+            val title = edtReadActivity.text.toString()
+
+            val sharedPref: SharedPreferences = getSharedPreferences(com.example.wit_project.Pref_Name, com.example.wit_project.PRIVATE_MODE)
+            val data = sharedPref.getString(title,"no data for this title")
+
+            tvData.text = data
+        }
     }
 }
